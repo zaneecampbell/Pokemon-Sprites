49874d003e53ff785facce58e052807c4cb0b5ad
Zane Campbell <zanegitinit@gmail.com>
2019-10-04 22:08:31 -0700

    first commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WeatherApp from '../src/components/WeatherApp';
+import LandingPage from '../src/components/LandingPage';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true  // updates material-ui typography automatically when next major release happens (Q1 2019)
+window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true; // updates material-ui typography automatically when next major release happens (Q1 2019)
 
-const jsx = (
-    <WeatherApp />
-);
+const jsx = <LandingPage />;
 
 ReactDOM.render(jsx, document.getElementById('app'));
